14c0dd73425d7784ac2ee7eed87d71400ca3a44e
a-s-t-e-y-a <krishnoit100@gmail.com>
2026-02-04 19:54:50 +0530

    FEAT: fix next config for image optimization

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
 
   images: {
     unoptimized: false,
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL:86400,
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       {
         protocol: 'https',
@@ -33,9 +37,6 @@ const nextConfig: NextConfig = {
         hostname: 'cdn.shopify.com',
       },
     ],
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 
   async headers() {
